1e22f9452cfbeab838cb5298b30194ef125a73eb
rthaqori <ramanthaqori@gmail.com>
2024-08-26 19:46:16 +0545

    commit

--- src/Components/NavComponents/Header.jsx
+++ src/Components/NavComponents/Header.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect, useRef } from "react";
-import Container from "../Utils/Container";
 import PersonIcon from "@mui/icons-material/Person";
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
@@ -80,100 +79,98 @@ const HeaderNav = () => {
   }, []);
 
   return (
-    <Container>
-      <div className="flex h-16 w-full items-center justify-between bg-white px-6 py-2 shadow-md md:px-7 md:py-3 lg:px-9 lg:py-4">
-        {/* Mobile Menu Button */}
-        <div className="md:hidden">
-          <button onClick={toggleMenu} aria-label="Toggle menu">
-            <MenuIcon />
-          </button>
-        </div>
-
-        {/* Logo */}
-        <div className="flex items-center text-3xl font-bold lg:text-4xl">
-          <Link to="/">rthaqori</Link>
-        </div>
-
-        {/* Mobile Menu */}
-        <div
-          className={`absolute top-0 h-full w-4/5 bg-white px-10 py-5 shadow-lg transition-all duration-300 ease-in-out md:relative md:flex md:h-fit md:w-fit md:px-0 md:py-0 md:shadow-none ${
-            open ? "left-0" : "-left-full md:left-0"
-          }`}
-        >
-          <div className="flex h-10 items-center justify-end md:hidden">
-            <button onClick={toggleMenu} aria-label="Close menu">
-              <CloseIcon />
-            </button>
-          </div>
-          <ul className="mt-5 flex flex-col space-y-6 text-2xl font-semibold md:m-0 md:flex-row md:items-center md:space-x-6 md:space-y-0 md:text-lg">
-            <li className="border-orange-600 transition-all duration-100 ease-in-out hover:border-b-2">
-              <Link
-                to="/"
-                className="capitalize text-black transition-colors duration-300 ease-in-out hover:text-gray-600"
-              >
-                home
-              </Link>
-            </li>
-            <li className="border-orange-600 transition-all duration-100 ease-in-out hover:border-b-2">
-              <Link
-                to="/shop"
-                className="capitalize text-black transition-colors duration-300 ease-in-out hover:text-gray-600"
-              >
-                shop
-              </Link>
-            </li>
-            <li className="border-orange-600 transition-all duration-100 ease-in-out hover:border-b-2">
-              <Link className="capitalize text-black transition-colors duration-300 ease-in-out hover:text-gray-600">
-                about
-              </Link>
-            </li>
-            <li className="border-orange-600 transition-all duration-100 ease-in-out hover:border-b-2">
-              <Link className="capitalize text-black transition-colors duration-300 ease-in-out hover:text-gray-600">
-                blog
-              </Link>
-            </li>
-            <li className="border-orange-600 transition-all duration-100 ease-in-out hover:border-b-2">
-              <Link className="capitalize text-black transition-colors duration-300 ease-in-out hover:text-gray-600">
-                contact
-              </Link>
-            </li>
-          </ul>
-        </div>
-
-        {/* Icons */}
-        <div className="flex items-center space-x-4">
-          <Link
-            to="/wishlist"
-            className="relative py-1 font-bold transition-colors duration-300 ease-in-out hover:text-orange-600"
-            aria-label="Wishlist"
-          >
-            {favQuantity > 0 && (
-              <span className="absolute -right-1/4 -top-1/4 flex h-5 w-5 translate-x-1/4 translate-y-1/4 items-center justify-center rounded-full bg-[#111111] text-[10px] font-medium text-white">
-                {favQuantity}
-              </span>
-            )}
-            <FavoriteBorderIcon />
-          </Link>
-          <Link
-            to="/cart"
-            className="relative py-1 font-bold transition-colors duration-300 ease-in-out hover:text-orange-600"
-            aria-label="Shopping Cart"
-          >
-            {bagQuantity > 0 && (
-              <span className="absolute -right-1/4 -top-1/4 flex h-5 w-5 translate-x-1/4 translate-y-1/4 items-center justify-center rounded-full bg-[#111111] text-[10px] font-medium text-white">
-                {bagQuantity}
-              </span>
-            )}
-            <ShoppingCartOutlinedIcon />
-          </Link>
-          <button
-            className="py-1 font-bold transition-colors duration-300 ease-in-out hover:text-orange-600"
-            aria-label="Account"
-          >
-            <PersonIcon />
-          </button>
-        </div>
+    <div className="flex h-16 w-full items-center justify-between bg-white px-6 py-2 shadow-md md:px-7 md:py-3 lg:px-9 lg:py-4">
+      {/* Mobile Menu Button */}
+      <div className="md:hidden">
+        <button onClick={toggleMenu} aria-label="Toggle menu">
+          <MenuIcon />
+        </button>
       </div>
-    </Container>
+
+      {/* Logo */}
+      <div className="flex items-center text-3xl font-bold lg:text-4xl">
+        <Link to="/">rthaqori</Link>
+      </div>
+
+      {/* Mobile Menu */}
+      <div
+        className={`absolute top-0 h-full w-4/5 bg-white px-10 py-5 shadow-lg transition-all duration-300 ease-in-out md:relative md:flex md:h-fit md:w-fit md:px-0 md:py-0 md:shadow-none ${
+          open ? "left-0" : "-left-full md:left-0"
+        }`}
+      >
+        <div className="flex h-10 items-center justify-end md:hidden">
+          <button onClick={toggleMenu} aria-label="Close menu">
+            <CloseIcon />
+          </button>
+        </div>
+        <ul className="mt-5 flex flex-col space-y-6 text-2xl font-semibold md:m-0 md:flex-row md:items-center md:space-x-6 md:space-y-0 md:text-lg">
+          <li className="border-orange-600 transition-all duration-100 ease-in-out hover:border-b-2">
+            <Link
+              to="/"
+              className="capitalize text-black transition-colors duration-300 ease-in-out hover:text-gray-600"
+            >
+              home
+            </Link>
+          </li>
+          <li className="border-orange-600 transition-all duration-100 ease-in-out hover:border-b-2">
+            <Link
+              to="/shop"
+              className="capitalize text-black transition-colors duration-300 ease-in-out hover:text-gray-600"
+            >
+              shop
+            </Link>
+          </li>
+          <li className="border-orange-600 transition-all duration-100 ease-in-out hover:border-b-2">
+            <Link className="capitalize text-black transition-colors duration-300 ease-in-out hover:text-gray-600">
+              about
+            </Link>
+          </li>
+          <li className="border-orange-600 transition-all duration-100 ease-in-out hover:border-b-2">
+            <Link className="capitalize text-black transition-colors duration-300 ease-in-out hover:text-gray-600">
+              blog
+            </Link>
+          </li>
+          <li className="border-orange-600 transition-all duration-100 ease-in-out hover:border-b-2">
+            <Link className="capitalize text-black transition-colors duration-300 ease-in-out hover:text-gray-600">
+              contact
+            </Link>
+          </li>
+        </ul>
+      </div>
+
+      {/* Icons */}
+      <div className="flex items-center space-x-4">
+        <Link
+          to="/wishlist"
+          className="relative py-1 font-bold transition-colors duration-300 ease-in-out hover:text-orange-600"
+          aria-label="Wishlist"
+        >
+          {favQuantity > 0 && (
+            <span className="absolute -right-1/4 -top-1/4 flex h-5 w-5 translate-x-1/4 translate-y-1/4 items-center justify-center rounded-full bg-[#111111] text-[10px] font-medium text-white">
+              {favQuantity}
+            </span>
+          )}
+          <FavoriteBorderIcon />
+        </Link>
+        <Link
+          to="/cart"
+          className="relative py-1 font-bold transition-colors duration-300 ease-in-out hover:text-orange-600"
+          aria-label="Shopping Cart"
+        >
+          {bagQuantity > 0 && (
+            <span className="absolute -right-1/4 -top-1/4 flex h-5 w-5 translate-x-1/4 translate-y-1/4 items-center justify-center rounded-full bg-[#111111] text-[10px] font-medium text-white">
+              {bagQuantity}
+            </span>
+          )}
+          <ShoppingCartOutlinedIcon />
+        </Link>
+        <button
+          className="py-1 font-bold transition-colors duration-300 ease-in-out hover:text-orange-600"
+          aria-label="Account"
+        >
+          <PersonIcon />
+        </button>
+      </div>
+    </div>
   );
 };
